1573dc037504a7df324fd4cdbe60a379d49243af
Jaime Muñoz <jaimemmp@gmail.com>
2013-12-09 11:52:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iappointment::Application.config.secret_key_base = 'b0aec6a6c677971342d7177d1635ccd636822cfa673bdde7a5e12fbde2cb8f9dfd15813262df811815b8516889481bd44f10348cdc39ab8355dcc90558b09d6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Iappointment::Application.config.session_store :cookie_store, key: '_iappointment_session'
